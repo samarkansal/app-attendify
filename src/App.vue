946a2b697af69802b96075a209dead3bf230b225
Samar Kansal <samarkansal@Samars-MacBook-Air.local>
2023-02-21 17:39:02 -0500

    MAINT: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView, useRoute } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import { provide } from "vue";
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+const route = useRoute();
+provide("categoryList", categoryList);
+</script>
+
+<template>
+  <div id="app">
+    <the-header></the-header>
+    <router-view :key="route.fullPath"></router-view>
+    <the-footer></the-footer>
+  </div>
+</template>
